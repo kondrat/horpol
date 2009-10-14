53838e9781f2921b23b57788ae7f966a229c85af
unknown <akv@.air.local>
2009-10-14 17:30:06 +0400

    subCatIndex
    Signed-off-by: unknown <akv@.air.local>

--- webroot/js/SubCatIndex.js
+++ webroot/js/SubCatIndex.js
@@ -25,42 +25,6 @@ $(document).ready( function(){
     			$('#stepIcon3').css({'background-position':bgPosition3}); 
     	}); 
  
- 
- /* 	 
-			$('#step1 .activeStep').hover( function(){
-    			$('#stepIcon1').css({'background-position':'0 0px'});
-    			},function() {
-    			$('#stepIcon1').css({'background-position':'0 -40px'});    	
-    		});
-	*/
-
-   /* 
-    $('.activeStep #brandSelect').hover(function(){
-    	$('#stepIcon2').css({'background-position':'-20px 0px'});
-    },function() {
-    	$('#stepIcon2').css({'background-position':'-20px -40px'});    	
-    });
-    
-     $('.activeStep #subcatSelect').hover(function(){
-    	$('#stepIcon3').css({'background-position':'-40px 0px'});
-    },function() {
-    	$('#stepIcon3').css({'background-position':'-40px -40px'});    	
-    });
-       
-    $('.prevStep').hover(function(){
-    	$('#stepIcon1').css({'background-position':'0 0'});
-    },function() {
-    	$('#stepIcon1').css({'background-position':'0 -20px'});    	
-    });
-    
-    
-    $('.prevStep #brandSelect').hover(function(){
-    	$('#stepIcon2').css({'background-position':'-20px 0'});
-    },function() {
-    	$('#stepIcon2').css({'background-position':'-20px -20px'});    	
-    });   
-    
-    */ 
     $('.disableStep a').click(function(){
     	return false;
     });
